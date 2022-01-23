29c46cfb8db0978b88d5a214320f3bc4b5e1185b
ilikefoodonly <89000017+ilikefoodonly@users.noreply.github.com>
2022-01-23 13:30:38 +0530

    Add files via upload

---
+++ new bspwm/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config &
+polybar -q music -c "$DIR"/config &
+polybar -q extra -c "$DIR"/config &
+polybar -q xwindow -c "$DIR"/config &
+# IPC settings and test
+ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
+echo message >/tmp/ipc-main
